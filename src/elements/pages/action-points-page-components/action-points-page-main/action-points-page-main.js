8f13948a3b464aaac892aac5a9b6f6119c53f07b
Bogdan Poslovsky <bogdan.p@razortheory.com>
2018-05-18 13:27:04 +0300

    Redirect default to list

--- src/elements/pages/action-points-page-components/action-points-page-main/action-points-page-main.js
+++ src/elements/pages/action-points-page-components/action-points-page-main/action-points-page-main.js
@@ -10,6 +10,18 @@ class ActionPointsPageMain extends Polymer.Element {
             routeData: Object
         };
     }
+
+    static get observers() {
+        return [
+            '_setRoutePath(route.path)'
+        ];
+    }
+
+    _setRoutePath(path) {
+        if (!path.length) {
+            this.set('route.path', '/list');
+        }
+    }
 }
 
 window.customElements.define(ActionPointsPageMain.is, ActionPointsPageMain);
