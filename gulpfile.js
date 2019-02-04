4f76d8435baeed8a597087a9e281500d6dd54825
drzackyll <adams.z.d@gmail.com>
2019-02-04 13:30:23 -0500

    Converts javascript files to typescript

--- gulpfile.js
+++ gulpfile.js
@@ -8,57 +8,10 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-// const gulp = require('gulp');
-// const rename = require('gulp-rename');
-// const replace = require('gulp-replace');
-// const del = require('del');
-// const spawn = require('child_process').spawn;
-
-// /**
-//  * Cleans the prpl-server build in the server directory.
-//  */
-// gulp.task('prpl-server:clean', () => {
-//   return del('server/build');
-// });
-
-// /**
-//  * Copies the prpl-server build to the server directory while renaming the
-//  * node_modules directory so services like App Engine will upload it.
-//  */
-// gulp.task('prpl-server:build', () => {
-//   const pattern = 'node_modules';
-//   const replacement = 'node_assets';
-
-//   return gulp.src('build/**')
-//     .pipe(rename(((path) => {
-//       path.basename = path.basename.replace(pattern, replacement);
-//       path.dirname = path.dirname.replace(pattern, replacement);
-//     })))
-//     .pipe(replace(pattern, replacement))
-//     .pipe(gulp.dest('server/build'));
-// });
-
-// gulp.task('prpl-server', gulp.series(
-//   'prpl-server:clean',
-//   'prpl-server:build'
-// ));
-
-// /**
-//  * Gulp task to run `tsc --watch` and `polymer serve` in parallel.
-//  */
-// gulp.task('serve', () => {
-//   const spawnOptions = {
-//     // `shell` option for Windows compatability. See:
-//     // https://nodejs.org/api/child_process.html#child_process_spawning_bat_and_cmd_files_on_windows
-//     shell: true,
-//     stdio: 'inherit'
-//   };
-//   spawn('tsc', ['--watch'], spawnOptions);
-//   spawn('polymer', ['serve'], spawnOptions);
-// });
-
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json')
 
 const clean = require('./gulp-tasks/clean');
 const path = require('path');
@@ -96,7 +49,12 @@ gulp.task('prodBuild', gulp.series(clean, preBuild, build, postBuild));
 
 gulp.task('devup', gulp.series('devBuild', gulp.parallel('startServer', 'watch')));
 
-gulp.task('default', gulp.series(['prodBuild']));
+// gulp.task('default', gulp.series(['prodBuild']));
+gulp.task('default', function() {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest('dist'));
+});
 
 
 // const gulp = require('gulp');
