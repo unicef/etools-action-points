1fb1c13579fd64bc6ee747f8f2c05c28b8b6429a
Trif Adriana <trif.corina.adriana@gmail.com>
2023-04-20 20:14:12 +0300

    Add Language dropdown

---
+++ src_ts/config/app-constants.ts
@@ -0,0 +1,1 @@
+export const appLanguages: {value: string; display_name: string}[] = [{value: 'en', display_name: 'English'}];
--- src_ts/elements/app-shell-components/app-main-header/app-main-header.ts
+++ src_ts/elements/app-shell-components/app-main-header/app-main-header.ts
@@ -12,13 +12,20 @@ import {customElement, property, observe} from '@polymer/decorators';
 import {GenericObject} from '../../../typings/globals.types';
 import MatomoMixin from '@unicef-polymer/etools-piwik-analytics/matomo-mixin';
 import {DexieRefresh} from '@unicef-polymer/etools-utils/dist/singleton/dexie-refresh';
+import {appLanguages} from '../../../config/app-constants';
+import '@unicef-polymer/etools-dropdown/etools-dropdown';
+import {use} from 'lit-translate';
+import {sendRequest} from '@unicef-polymer/etools-ajax';
+import {parseRequestErrorsAndShowAsToastMsgs} from '@unicef-polymer/etools-ajax/ajax-error-parser';
+import apdEndpoints from '../../../endpoints/endpoints';
+import {UserControllerMixin} from '../../mixins/user-controller';
 
 /**
  * @polymer
  * @customElement
  */
 @customElement('app-main-header')
-export class AppMainHeader extends MatomoMixin(PolymerElement) {
+export class AppMainHeader extends MatomoMixin(UserControllerMixin(PolymerElement)) {
   public static get template() {
     return html`
       ${sharedStyles}
@@ -42,7 +49,25 @@ export class AppMainHeader extends MatomoMixin(PolymerElement) {
           color: var(--light-secondary-text-color);
           margin: 0 16px;
         }
+        .dropdowns {
+          display: flex;
+          margin-inline-end: 5px;
+        }
 
+        .header {
+          flex-wrap: wrap;
+          height: 100%;
+          justify-content: space-between;
+        }
+
+        .header__item {
+          display: flex;
+          align-items: center;
+        }
+
+        .header__right-group {
+          justify-content: space-evenly;
+        }
         .titlebar {
           flex: 1;
           font-size: 28px;
@@ -67,10 +92,36 @@ export class AppMainHeader extends MatomoMixin(PolymerElement) {
           font-weight: 700;
           font-size: 18px;
         }
+        etools-dropdown {
+          --paper-listbox: {
+            max-height: 600px;
+          }
+
+          --esmm-icons: {
+            color: var(--light-secondary-text-color);
+            cursor: pointer;
+          }
+
+          --paper-input-container-underline: {
+            display: none;
+          }
+
+          --paper-input-container-underline-focus: {
+            display: none;
+          }
+
+          --paper-input-container-shared-input-style: {
+            color: var(--light-secondary-text-color);
+            cursor: pointer;
+            font-size: 16px;
+            text-align: right;
+            width: 100px;
+          }
+        }
       </style>
 
-      <app-toolbar sticky class="content-align">
-        <div class="titlebar content-align">
+      <app-toolbar sticky class="content-align header">
+        <div class="header__item">
           <etools-app-selector user="[[user]]"></etools-app-selector>
 
           <img src$="[[rootPath]]../../../../../../apd/images/etools-logo-color-white.svg" />
@@ -79,9 +130,25 @@ export class AppMainHeader extends MatomoMixin(PolymerElement) {
           </template>
         </div>
 
-        <div class="content-align">
-          <countries-dropdown countries="[[user.countries_available]]" country-id="[[user.country.id]]">
-          </countries-dropdown>
+        <div class="header__item header__right-group">
+          <div class="dropdowns">
+            <etools-dropdown
+              id="languageSelector"
+              selected="[[user.preferences.language]]"
+              options="[[appLanguages()]]"
+              option-label="display_name"
+              option-value="value"
+              on-etools-selected-item-changed="languageChanged"
+              trigger-value-change-event
+              hide-search
+              allow-outside-scroll
+              no-label-float
+              auto-width
+              placeholder="Language"
+            ></etools-dropdown>
+            <countries-dropdown countries="[[user.countries_available]]" country-id="[[user.country.id]]">
+            </countries-dropdown>
+          </div>
 
           <support-btn title="Support"></support-btn>
 
@@ -117,6 +184,43 @@ export class AppMainHeader extends MatomoMixin(PolymerElement) {
   connectedCallback() {
     super.connectedCallback();
     this.addEventListener('sign-out', this._logout);
+  }
+
+  languageChanged(e: CustomEvent): void {
+    if (!e.detail.selectedItem) {
+      return;
+    }
+
+    const newLanguage = e.detail.selectedItem.value;
+    if (newLanguage) {
+      window.dayjs.locale(newLanguage);
+      // Event caught by self translating npm packages
+      this.dispatchEvent(
+        new CustomEvent('language-changed', {
+          detail: {language: newLanguage}
+        })
+      );
+    }
+    if (newLanguage !== this.user.preferences.language) {
+      localStorage.setItem('defaultLanguage', newLanguage);
+      use(newLanguage).then(() => {
+        if (this.user?.preferences?.language != newLanguage) {
+          this.updateUserPreference(newLanguage);
+        }
+      });
+    }
+  }
+
+  appLanguages() {
+    return appLanguages;
+  }
+
+  private updateUserPreference(language: string) {
+    sendRequest({endpoint: apdEndpoints.userProfile, method: 'PATCH', body: {preferences: {language: language}}})
+      .then((response) => {
+        this._setUserData(response);
+      })
+      .catch((err: any) => parseRequestErrorsAndShowAsToastMsgs(err, this));
   }
 
   openDrawer() {
